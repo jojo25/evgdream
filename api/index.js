29be12ff2dac929f76ff54afe10598559174f721
Jonas Latapie <jonaslatapie@gmail.com>
2022-05-18 17:45:47 +0200

    validation devis

--- api/index.js
+++ api/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mysql = require('mysql');
 const bcrypt = require('bcrypt');
+const nodemailer = require("nodemailer");
+const path = require('path');
+const hbs = require('nodemailer-express-handlebars')
 
 const db = mysql.createConnection({
     host: "localhost",
@@ -81,7 +84,8 @@ app.post('/creationcompte', (req,res) => {
             db.query("INSERT INTO `comptes` VALUES (NULL, ?, ?, ?);", 
             [ req.body.email, hash, req.body.telephone ], function (err, result) {
                 if (err) throw err;
-                console.log(result)
+
+
                 res.status(200).json(result[0])
             });
         });
@@ -111,6 +115,72 @@ app.get('/connexioncompte/:email/:password', (req,res) => {
     });
            
 })
+
+app.post('/validationdevis', (req,res) => {
+
+    db.query("INSERT INTO `devis` VALUES (NULL, ?, ?, ?, ?, ?, ?, ?)", [ req.body.date_depart, req.body.date_retour, req.body.ville_depart, req.body.destination, req.body.nb_participant, req.body.budget, req.body.details ], function (err, result) {
+        if (err) throw err;
+        let activites = [];
+
+        req.body.activites.forEach( function (activite) {
+            activites.push([result.insertId, parseInt(activite.id)]);
+        });
+
+        db.query("INSERT INTO `devis_activite` (id_devis, id_activite) VALUES ?;", [activites], function(err, result) {
+            if (err) throw err;
+
+            // create reusable transporter object using the default SMTP transport
+            let transporter = nodemailer.createTransport({
+                host: "ssl0.ovh.net",
+                port: 465,
+                secure: true, // true for 465, false for other ports
+                auth: {
+                    user: "contact@evgdream.com", // generated ethereal user
+                    pass: "Benjipsgemailevgdream00!", // generated ethereal password
+                }
+            });
+
+            // point to the template folder
+            const handlebarOptions = {
+                viewEngine: {
+                    partialsDir: path.resolve('./views/'),
+                    defaultLayout: false
+                },
+                viewPath: path.resolve('./views/')
+            };
+
+            // use a template file with nodemailer
+            transporter.use('compile', hbs(handlebarOptions));
+
+            // send mail with defined transport object
+            let mailOptions = {
+                from: '"EVGDREAM" <contact@evgdream.com>', // sender address
+                to: "jonaslatapie@gmail.com", // list of receivers
+                subject: "Nouvelle Demande de devis de" + req.body.mail,  // Subject line
+                template: 'devis',
+                context: {
+                    mail: req.body.mail,
+                    tel: req.body.tel,
+                    date_depart: req.body.date_depart,
+                    date_retour: req.body.date_retour,
+                    ville_depart: req.body.ville_depart,
+                    destination: req.body.destination,
+                    nb_participant: req.body.nb_participant,
+                    activites: req.body.activites,
+                    budget: req.body.budget,
+                    message: req.body.message
+                }
+            };
+
+            transporter.sendMail(mailOptions, function(error, info){
+                if(error){
+                    return console.log(error);
+                }
+                console.log('Message sent: ' + info.response);
+            });
+        });
+    });
+});
 
 app.listen(8080, () => {
     console.log('Serveur à lécoute');
