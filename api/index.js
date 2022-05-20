ebdfa65acb0d54e67a1a426f29f7927c82ae2e5a
Jonas Latapie <jonaslatapie@gmail.com>
2022-05-20 11:11:20 +0200

    envoi mdp oublié

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const nodemailer = require("nodemailer");
 const path = require('path');
 const hbs = require('nodemailer-express-handlebars');
 const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
 
 const db = mysql.createConnection({
     host: "localhost",
@@ -117,6 +118,76 @@ app.get('/connexioncompte/:email/:password', (req,res) => {
            
 })
 
+
+
+app.post('/sendpassword', (req,res) => {
+    
+    let mail = req.body.mail;
+    let token = jwt.sign({mail: mail}, crypto.randomBytes(64).toString('hex'), { expiresIn: '86400s' });
+
+    db.query("SELECT * FROM comptes where mail = ?;", [ mail ], function (err, result) {
+        if (err) throw err;
+
+        if (result.length > 0) {
+            let id_personne = result[0].id;
+
+            let todayDate = new Date();
+            let todayDateSql = getSqlDate(todayDate);
+
+            let tomorrowDate = new Date(todayDate);
+            tomorrowDate.setDate(tomorrowDate.getDate() + 1)
+            let tomorrowDateSql = getSqlDate(tomorrowDate);
+
+            db.query("INSERT INTO `token_password` VALUES (NULL, ?, ?, ?, ?)", [ id_personne, token, todayDateSql, tomorrowDateSql], function (err, result) {
+                
+                if (err) throw err;
+
+                // create reusable transporter object using the default SMTP transport
+                let transporter = nodemailer.createTransport({
+                    host: "ssl0.ovh.net",
+                    port: 465,
+                    secure: true, // true for 465, false for other ports
+                    auth: {
+                        user: "contact@evgdream.com", // generated ethereal user
+                        pass: "Benjipsgemailevgdream00!", // generated ethereal password
+                    }
+                });
+
+                // point to the template folder
+                const handlebarOptions = {
+                    viewEngine: {
+                        partialsDir: path.resolve('./views/'),
+                        defaultLayout: false
+                    },
+                    viewPath: path.resolve('./views/')
+                };
+
+                // use a template file with nodemailer
+                transporter.use('compile', hbs(handlebarOptions));
+
+                // send mail with defined transport object
+                let mailOptions = {
+                    from: '"EVGDREAM" <contact@evgdream.com>', // sender address
+                    to: req.body.mail, // list of receivers
+                    subject: "Réinitialisation de votre mot de passe",  // Subject line
+                    template: 'sendpassword',
+                    context: {
+                        mail: req.body.mail,
+                        token: token
+                    }
+                };
+
+                transporter.sendMail(mailOptions, function(err, info){
+                    if (err) throw err;
+                    res.status(200).json(result[0]);
+                });
+            });
+        }
+        
+        
+    });
+});
+
 app.post('/validationdevis', (req,res) => {
 
     db.query("INSERT INTO `devis` VALUES (NULL, ?, ?, ?, ?, ?, ?, ?)", [ req.body.date_depart, req.body.date_retour, req.body.ville_depart, req.body.destination, req.body.nb_participant, req.body.budget, req.body.details ], function (err, result) {
@@ -182,6 +253,17 @@ app.post('/validationdevis', (req,res) => {
         });
     });
 });
+
+var getSqlDate = (date) => {
+    let dd = String(date.getDate()).padStart(2, '0');
+    let mm = String(date.getMonth() + 1).padStart(2, '0'); //January is 0!
+    let yyyy = date.getFullYear();
+    let time = date.toLocaleTimeString();
+
+    return yyyy + '-' + mm + '-' + dd + ' ' + time;
+}
+
+
 
 app.listen(8080, () => {
     console.log('Serveur à lécoute');
